b29bda96c7b1740e2618cc2a853c82f137d43105
Georgia <ghobs987@gmail.com>
2022-11-13 01:53:09 +0000

    basic styling in language

--- pages/components/buildcharactercomponents/cardcomponents/languagecomponents/Language.tsx
+++ pages/components/buildcharactercomponents/cardcomponents/languagecomponents/Language.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Language() {
-  return <div>Dialects</div>;
+import { languageInterface } from "../../../../../interfaces/templateInterface";
+
+import styles from "../../../../../styles/Home.module.css";
+
+export default function Language(Props: languageInterface) {
+  const { language, written, spoken, inflection, proficiency, dialect } = Props;
+
+  const [show, setShow] = useState(false);
+
+  return (
+    <div
+      className="flex justify-center text-center flex-row"
+      onClick={() => setShow(!show)}
+    >
+      <div className="w-6 h-6 bg-white border-solid border-black rounded-full">
+        <div
+          className={styles.arrowdown}
+          style={{
+            transform: show ? "rotate(225deg)" : "",
+            transition: "transform 150ms ease",
+          }}
+        />
+      </div>
+      <div className="border-white border-2 rounded-full">{language}</div>
+      <div className="w-6 h-6 bg-white border-solid border-black rounded-full">
+        <div
+          className={styles.arrowdown}
+          style={{
+            transform: show ? "rotate(225deg)" : "",
+            transition: "transform 150ms ease",
+          }}
+        />
+      </div>
+    </div>
+  );
 }
