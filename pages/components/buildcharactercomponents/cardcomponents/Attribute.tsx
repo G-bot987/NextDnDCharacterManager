7def351d58f6e91e135c442e10bdd0c227928e5f
Georgia <ghobs987@gmail.com>
2022-12-07 23:19:18 +0000

    styling consistent with languages

--- pages/components/buildcharactercomponents/cardcomponents/Attribute.tsx
+++ pages/components/buildcharactercomponents/cardcomponents/Attribute.tsx
@@ -7,31 +7,44 @@ import { attributesInterface } from "../../../../interfaces/templateInterface";
 export default function Attribute(Props: attributesInterface) {
   const { attribute } = Props;
 
+  console.log("props ");
+  console.log(Props);
+  console.log("---");
+
   const [show, setShow] = useState(false);
 
   return (
-    <li className="flex flex-row justify-center text-center border-solid border-white border-2 rounded-t-lg">
-      <div className="w-6 h-6 bg-white border-solid border-black rounded-full">
-        <div
-          onClick={() => setShow(!show)}
-          className={styles.arrowdown}
-          style={{
-            transform: show ? "rotate(225deg)" : "",
-            transition: "transform 150ms ease",
-          }}
-        />
+    <li className="flex flex-row justify-center text-center ">
+      <div onClick={() => setShow(!show)} className="flex flex-row">
+        <div className="w-6 h-6 bg-white border-solid border-black rounded-full">
+          <div
+            className={styles.arrowdown}
+            style={{
+              transform: show ? "rotate(225deg)" : "",
+              transition: "transform 150ms ease",
+            }}
+          />
+        </div>
+        <div className="border-solid border-white border-2 rounded-t-lg">
+          {attribute}
+        </div>
+        <div className="w-6 h-6 bg-white border-solid border-black rounded-full">
+          <div
+            className={styles.arrowdown}
+            style={{
+              transform: show ? "rotate(225deg)" : "",
+              transition: "transform 150ms ease",
+            }}
+          />
+        </div>
       </div>
-      {attribute}
-      <div className="w-6 h-6 bg-white border-solid border-black rounded-full">
-        <div
-          onClick={() => setShow(!show)}
-          className={styles.arrowdown}
-          style={{
-            transform: show ? "rotate(225deg)" : "",
-            transition: "transform 150ms ease",
-          }}
-        />
-      </div>
+      {show && (
+        <ul className="flex flex-col justify-around  text-center  space-y-4">
+          {/* {varients.map((varient: languageInterface, index: number) => (
+            <Language {...varient} key={index} />
+          ))} */}
+        </ul>
+      )}
     </li>
   );
 }
