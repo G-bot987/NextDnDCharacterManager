185fef136f4c0bc90837ddb15d26df57d43d169c
Georgia <ghobs987@gmail.com>
2022-12-07 23:46:00 +0000

    restructure

---
+++ pages/components/buildcharactercomponents/cardcomponents/attributecomponents/skillcomponents/Myskills.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { skillInterface } from "../../../../../../interfaces/templateInterface";
+
+export default function Myskills(Props: skillInterface) {
+  return <div>Myskills</div>;
+}
