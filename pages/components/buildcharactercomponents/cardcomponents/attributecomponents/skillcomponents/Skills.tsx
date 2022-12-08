33e330e30d0529aa8bae97dcbab1fd26764c0f4e
Georgia <ghobs987@gmail.com>
2022-12-08 22:38:13 +0000

    using usestate and use effect to change value of proficiency

--- pages/components/buildcharactercomponents/cardcomponents/attributecomponents/skillcomponents/Skills.tsx
+++ pages/components/buildcharactercomponents/cardcomponents/attributecomponents/skillcomponents/Skills.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { skillInterface } from "../../../../../../interfaces/templateInterface";
+import Skillproficiency from "./Skillproficiency";
 
 export default function Skills(Props: any) {
   const { skill } = Props;
   var { proficiency } = Props;
 
   const skillName = skill.skill;
+  const [select, setSelect] = useState(false);
+
+  useEffect(() => {
+    if (select === true) {
+      proficiency = true;
+    } else {
+      proficiency = false;
+    }
+  });
+
   return (
-    <li className="flex flex-col">
+    <li className="flex flex-col" onClick={() => setSelect(!select)}>
       <p>{skillName}</p>
+      <Skillproficiency />
     </li>
   );
 }
