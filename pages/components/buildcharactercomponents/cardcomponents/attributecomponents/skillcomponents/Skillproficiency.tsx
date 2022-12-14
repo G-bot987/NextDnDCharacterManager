db4c606b2a4f39056b32c6f3bd2865f0fb4d1277
Georgia <ghobs987@gmail.com>
2022-12-14 23:40:13 +0000

    build fix

--- pages/components/buildcharactercomponents/cardcomponents/attributecomponents/skillcomponents/Skillproficiency.tsx
+++ pages/components/buildcharactercomponents/cardcomponents/attributecomponents/skillcomponents/Skillproficiency.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 interface SkillproficiencyInterface {
-  proficiency?: boolean;
+  proficiency?: boolean | undefined | null;
   skillName?: string | null;
   attribute?: string;
 }
 
 export default function Skillproficiency(Props: SkillproficiencyInterface) {
   const { proficiency } = Props;
-  return <div>Skillproficiency</div>;
+  return <div>Skillproficiency {proficiency}</div>;
 }
