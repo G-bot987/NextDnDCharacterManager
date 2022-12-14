d7e746a7136745940643e3f4713cb9955fadfa5f
Georgia <ghobs987@gmail.com>
2022-12-14 23:35:36 +0000

    will always be boolean

--- pages/components/buildcharactercomponents/cardcomponents/attributecomponents/skillcomponents/Skillproficiency.tsx
+++ pages/components/buildcharactercomponents/cardcomponents/attributecomponents/skillcomponents/Skillproficiency.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 interface SkillproficiencyInterface {
-  proficiency?: boolean | null | undefined;
+  proficiency?: boolean;
   skillName?: string | null;
   attribute?: string;
 }
 
 export default function Skillproficiency(Props: SkillproficiencyInterface) {
   const { proficiency } = Props;
-  return <div>Skillproficiency {proficiency}</div>;
+  return <div>Skillproficiency</div>;
 }
