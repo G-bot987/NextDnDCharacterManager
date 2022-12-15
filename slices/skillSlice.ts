f05baeb0e4aa30f1109b8888e2542133b5102156
Georgia <ghobs987@gmail.com>
2022-12-15 00:39:37 +0000

    rename singlur reducer for all attributes&skills

---
+++ slices/skillSlice.ts
@@ -0,0 +1,55 @@
+import { RootState } from "../store";
+
+export interface SkillProficiencyState {
+  skillArray: skillInterface[];
+}
+export interface skillInterface {
+  value: string;
+  selected?: boolean;
+}
+const initialState: SkillProficiencyState = {
+  skillArray: [],
+};
+
+// actions
+
+export const attributeValue = (attribute: any) => {
+  return {
+    type: "ATTRIBUTE_VALUE",
+    payload: attribute,
+  };
+};
+
+export const skillProTrue = (skill: any) => {
+  return {
+    type: "SKILL_PROFICIENCY_TRUE",
+    payload: skill,
+  };
+};
+
+export const skillProFalse = (skill: any) => {
+  return {
+    type: "SKILL_PROFICIENCY_FALSE",
+    payload: skill,
+  };
+};
+
+// reducers
+export function skillPropertiesReducer(
+  state = initialState,
+  action: any
+) {
+  switch (action.type) {
+    case "ATTRIBUTE_VALUE":
+
+    case "SKILL_PROFICIENCY_TRUE":
+
+    case "SKILL_PROFICIENCY_FALSE":
+
+    default:
+      return state;
+  }
+}
+
+export const rootState = (state: RootState) =>
+  state.languagePropertiesReducer.languagesArray;
