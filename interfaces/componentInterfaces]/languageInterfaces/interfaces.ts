518255586f8b50b4b31c9b305d1f75d847ae42ac
Georgia <ghobs987@gmail.com>
2022-12-17 16:38:34 +0000

    new interfaces for language property comps

---
+++ interfaces/componentInterfaces]/languageInterfaces/interfaces.ts
@@ -0,0 +1,23 @@
+export interface WrittenInterace {
+  written: boolean | null;
+  language: string;
+  dialect: string;
+}
+
+export interface SpokenInterace {
+  spoken: boolean | null;
+  language: string;
+  dialect: string;
+}
+
+export interface InflectionInterace {
+  inflection: boolean | null;
+  language: string;
+  dialect: string;
+}
+
+export interface ProficiencyInterace {
+  proficiency: boolean | null;
+  language: string;
+  dialect: string;
+}
