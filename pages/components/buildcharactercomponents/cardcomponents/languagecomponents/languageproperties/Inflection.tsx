1f2506913a4101fac6e1d7c1162da92dc804326c
Georgia <ghobs987@gmail.com>
2022-12-17 17:18:46 +0000

    new components for language properties

---
+++ pages/components/buildcharactercomponents/cardcomponents/languagecomponents/languageproperties/Inflection.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Inflection() {
+  return <div>Inflection</div>;
+}
---
+++ pages/components/buildcharactercomponents/cardcomponents/languagecomponents/languageproperties/Proficiency.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Proficiency() {
+  return <div>Proficiency</div>;
+}
