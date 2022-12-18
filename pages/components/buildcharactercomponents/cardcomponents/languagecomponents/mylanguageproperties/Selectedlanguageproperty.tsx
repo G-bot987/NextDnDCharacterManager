625a48fc55de1b11fde1e05257076799b5a67ac1
Georgia <ghobs987@gmail.com>
2022-12-18 21:03:58 +0000

    new structure and updated imports

---
+++ pages/components/buildcharactercomponents/cardcomponents/languagecomponents/mylanguageproperties/Selectedlanguageproperty.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+
+import { useDispatch } from "react-redux";
+import { LanguageProFalse } from "../../../../../../slices/languageSlice";
+
+import { ReduxLanguageInterface } from "../../../../../../interfaces/componentInterfaces/languageInterfaces/reduxInterfaces/reduxInterfaces";
+
+export default function Selectedlanguageproperty(
+  Props: ReduxLanguageInterface
+) {
+  const { selected, property, dialect, language } = Props;
+
+  const dispatch = useDispatch();
+  const [deselect, setDeselect] = useState(!selected);
+
+  useEffect(() => {
+    if (deselect === true) {
+      const reduxPayLoad = { property, selected: false, dialect, language };
+      dispatch(LanguageProFalse(reduxPayLoad));
+    }
+  }, [deselect]);
+
+  return (
+    <div className="flex justify-center text-center flex-row bg-white rounded-lg">
+      <div className="text-black">
+        {property} {dialect} {language}
+      </div>
+
+      <div
+        className="bg-black border-2 border-solid"
+        onClick={() => setDeselect(!deselect)}
+      >
+        <p className="text-white">Remove Proficiency</p>
+      </div>
+    </div>
+  );
+}
