c5838336ef77ecebfc8b6b6909d52f0c01dd09c5
Georgia <ghobs987@gmail.com>
2022-12-19 02:44:14 +0000

    attribute redux slice

---
+++ slices/attributesSlice.ts
@@ -0,0 +1,43 @@
+import { RootState } from "../store";
+
+import {
+  ReduxScoreInterface,
+  ReduxScoreProficiencyState,
+} from "../interfaces/componentInterfaces/scoreInterfaces/score";
+
+const initialState: ReduxScoreProficiencyState = {
+  attributesArray: [],
+};
+
+// actions
+
+export const attributeValue = (attribute: ReduxScoreInterface) => {
+  return {
+    type: "ATTRIBUTE_VALUE",
+    payload: attribute,
+  };
+};
+
+export function attributesReducer(state = initialState, action: any) {
+  switch (action.type) {
+    case "ATTRIBUTE_VALUE":
+      console.log("action payload");
+      console.log(action.payload);
+      console.log("---");
+      const storeData = state.attributesArray.filter(
+        (attributeScore) =>
+          attributeScore.attribute !== action.payload.attribute
+      );
+
+      return {
+        ...state,
+        attributesArray: [...storeData, action.payload],
+      };
+
+    default:
+      return state;
+  }
+}
+
+export const attributesRootState = (state: RootState) =>
+  state.attributesReducer.attributesArray;
