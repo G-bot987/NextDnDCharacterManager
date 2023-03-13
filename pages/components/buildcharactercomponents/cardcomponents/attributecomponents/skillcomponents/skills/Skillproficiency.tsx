25d6d3206232873681b7a0193d348d0849ce7750
gbot987 <ghobs987@gmail.com>
2023-03-13 01:21:21 +0000

    fix for deselecting skills rendering of pro number needs correcting

--- pages/components/buildcharactercomponents/cardcomponents/attributecomponents/skillcomponents/skills/Skillproficiency.tsx
+++ pages/components/buildcharactercomponents/cardcomponents/attributecomponents/skillcomponents/skills/Skillproficiency.tsx
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { skillProFalse, skillProTrue, skillRootState } from "../../../../../../../slices/skillSlice";
 
 interface SkillproficiencyInterface {
-  select: boolean;
-  skillName?: string | null;
-  attribute?: string;
+  skillName?: string;
+  attribute: string;
   mod: number;
+  proficiency: boolean
 }
 
 export default function Skillproficiency(Props: SkillproficiencyInterface) {
-  const { select, skillName, mod } = Props;
+
+  const { skillName, mod, attribute } = Props;
+  const store = useSelector(skillRootState);
+  const dispatch = useDispatch();
+
+  const inStore = store.find((element) => {
+    return element.skillName === skillName;
+  });
+
+  const select = inStore?.proficiency || false;
+
+  var [selected, setSelected] = useState(select);
+
+  useEffect(() => {
+
+    const skillProperty = { skillName, attribute, proficiency: selected, mod };
+
+    if (selected) {
+
+      dispatch(skillProTrue(skillProperty));
+    } else {
+      dispatch(skillProFalse(skillProperty))
+    }
+
+  }, [selected])
+
+  useEffect(() => {
+    if (!inStore.proficiency && selected) {
+      setSelected(!selected)
+    }
+  }, [inStore])
+
   return (
     <div
+      onClick={() => setSelected(!selected)}
       className="max-w-fit rounded-full border-2 border-white p-2"
       style={{
         background: select ? "white" : "",
