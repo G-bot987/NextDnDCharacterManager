b1ce9725b377ee03e52d1bb5ffa285b94066fb1d
gbot987 <ghobs987@gmail.com>
2023-03-13 23:55:42 +0000

    new items and weapons mock data for front end

---
+++ data/itemData.ts
@@ -0,0 +1,6 @@
+export const items = [
+    { name: 'gold', quantity: 0, weight: 1 },
+    { name: 'rations', quantity: 0, weight: 3 },
+    { name: 'winter travelling cloak', quantity: 0, weight: 8 }
+
+]
