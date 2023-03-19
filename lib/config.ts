2bd0a9fb24cac33d3b66b11c915a66a389572ded
gbot987 <ghobs987@gmail.com>
2023-03-19 19:25:11 +0000

    removing dotenv

--- lib/config.ts
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-export const config: {
-  DB?: string;
-  ITEMS_COLLECTION?: string;
-} = {
-  DB: process.env.DB,
-  ITEMS_COLLECTION: process.env.ITEMS_COLLECTION,
-};
